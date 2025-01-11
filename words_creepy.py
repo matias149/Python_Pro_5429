394f57fd6ba551ca9a3858ab8a351dd453b26227
matias149 <tanyasolv5@gmail.com>
2025-01-11 14:14:43 -0600

    Create words_creepy.py

---
+++ words_creepy.py
@@ -0,0 +1,17 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "OMG": "Expresion de asombro",
+            "NASHE": "Algo asombroso o divertido esta pasando",
+            "WTF": "Que carajo?"
+
+            }
+
+
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    print('Aqui esta el significado de tu palabra' meme_dict[world])  
+else:
+    print('Ya que no lo tengo,por que no usas Google?')
